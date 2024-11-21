b420956efb9678f7531673972a6949142eb452e4
Hapince <133777386+hapince@users.noreply.github.com>
2024-11-21 15:30:02 +0800

    1

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { TelegramProvider } from './context/TelegramContext';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <TelegramProvider>
+      <App />
+    </TelegramProvider>
+  </React.StrictMode>
+);
