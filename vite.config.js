ef2d03a8c7c8417e958b38891c68f2387ad326a2
Hapince <133777386+hapince@users.noreply.github.com>
2025-03-25 18:09:41 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/public/index.html',  // GitHub Pages 路径
+  base: '/landlords-game/',  // GitHub Pages 路径
   build: {
     outDir: 'dist'
   }
