337ca493330b2c79c190a1639c9c9b498fdbe286
Hapince <133777386+hapince@users.noreply.github.com>
2025-03-25 18:17:23 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/hapince/',  // GitHub Pages 路径
+  base: '/',  // GitHub Pages 路径
   build: {
     outDir: 'dist'
   }
